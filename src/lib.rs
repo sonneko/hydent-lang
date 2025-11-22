e5b34dd9db5f9e1d370102b7aca1d6586dfdd143
senpuki1do <senpuki1do@gmail.com>
2025-11-22 13:33:30 +0000

    add parser caller

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
 mod tokenizer;
+mod parser;
+
 pub use tokenizer::tokenizer::Tokenizer;
+pub use parser::parser::Parser;
 
 pub fn compile(program: &str) -> Result<(), String> {
     let tokenizer = Tokenizer::new(program);
-    let tokens = tokenizer.tokenize();
-    println!("{:?}", tokens);
+    let tokens = tokenizer.tokenize().unwrap();
+    
+    let ast = Parser::new(&tokens).parse().unwrap();
+
+    println!("{:?}", ast);
 
     unimplemented!();
 }
