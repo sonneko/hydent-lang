ffab636d7f3e43ebd87ec3181054798b7a2a81ed
senpuki1do <senpuki1do@gmail.com>
2025-12-08 12:02:38 +0000

    add comment to rust build script

--- build.rs
+++ build.rs
@@ -1,3 +1,9 @@
+/// The main function for the build script.
+///
+/// This build script is currently empty, but it can be used to
+/// perform various tasks before the main project is compiled,
+/// such as generating code, compiling C dependencies, or
+/// setting environment variables.
 fn main() {
     
 }
