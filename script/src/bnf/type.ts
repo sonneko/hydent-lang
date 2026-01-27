9d605a92649b6f2835362de86872449c51416044
senpuki1do <senpuki1do@gmail.com>
2026-01-27 09:45:52 +0000

    add reset scripts

--- script/src/bnf/type.ts
+++ script/src/bnf/type.ts
@@ -1,68 +1,44 @@
-// AST type definition of Bnf
-
-// <bnf> ::= { <character> "::=" <alternative> }
-// <alternative> ::= <term> { "|" <term> }
-// <term> ::= <factor> { <factor> }
-// <factor> ::= <character>
-//            | <terminal_string>
-//            | "[" <alternative> "]"
-//            | "{" <alternative> "}"
-//            | "(" <alternative> ")"
-// <character> ::= "<" <STRING> ">"
-// <terminal_string> ::= "\"" <STRING> "\""
-
-export type BNF_AST = {
-    rules: BNF_Rule[];
-}
-
-export type BNF_Rule = {
-    name: string;
-    alternative: BNF_Alternative;
-}
-
-export type BNF_Alternative = {
-    terms: BNF_Term[];
-}
-
-export type BNF_Term = {
-    factors: BNF_Factor[];
-}
-
-export type BNF_Factor = {
-    node_type: "character";
-    character: string;
-} | {
-    node_type: "terminal_string";
-    terminal_string: string;
-} | {
-    node_type: "option";
-    alternative: BNF_Alternative;
-} | {
-    node_type: "repeat";
-    alternative: BNF_Alternative;
-} | {
-    node_type: "group";
-    alternative: BNF_Alternative;
-}
-
-
-export type TokenType =
-    | "ASSIGN"          // ::=
-    | "PIPE"            // |
-    | "LBRACKET"        // [
-    | "RBRACKET"        // ]
-    | "LBRACE"          // {
-    | "RBRACE"          // }
-    | "LPAREN"          // (
-    | "RPAREN"          // )
-    | "SEMICOLON"       // ;
-    | "TERMINAL"        // "string"
-    | "NON_TERMINAL"    // <string>
-    | "EOF";
-
-export type Token = {
-    type: TokenType;
-    value: string;
-    line: number;
-    col: number;
-};
+// grammer of ebnf
+// ```ebnf
+// <ebnf> ::= { <rule> ";" } ;
+// -> fn parse_[$rule.character.name](&mut self) -> Return<[$rule.character.upper_name]> {
+//        [$rule.ret]
+//    }
+// 
+// <rule> ::= <character> "::=" <alternative> ;
+// -> [$alternative.ret]
+// 
+// <alternative> ::= <enum> | <struct> ;
+// -> $enum.ret | $struct.ret
+// 
+// 
+// <enum> ::= <enum_item> { "|" <enum_item> } ;
+// -> if let Ok([$enum_item.0]) = self.parse_[$enum.item.0.name]() {
+//        Ok([$enum.upper_name]::[$enum_item.terminal]([$enum_item.0.ret]))
+//    } else if let Ok([$enum_item.$i]) = self.parse_[$enum.item.$i.name]() {
+//        Ok([$enum.upper_name]::[$enum_item.terminal]([$enum_item.$1.ret]))
+//    } else {
+//        Err(ParseErr)
+//    }
+// 
+// <enum_item> ::= { <terminal> } <character> { <terminal> } "as" <terminal> | { <terminal> } "as" <terminal> ;
+// -> self.next(Token::[$token_name_map(terminal.$i.name)])?;
+//    let [$character.name] = self.parse_[$character.name]()?;
+//    self.next(Token::[$token_name_map(terminal.$i.name)])?;
+//    |
+//    self.next(Token::[$token_name_map(terminal.$i.name)])?;
+// 
+// 
+// <struct> ::= <struct_item> { <struct_item> } ;
+// -> self.parse_[$struct.item.$i.name]()?;
+// 
+// <struct_item> ::= "{" <character> "}" | "[" <character> "]" | <character> ;
+// -> let [$character.name] = self.ctx.arena.iter_with(Self::parse_[$character.name]);
+//    |
+//    let [$character.name] = self.parse_[$character.name]().into_option();
+//    |
+//    let [$character.name] = self.parse_[$character.name]()?;
+// 
+// <terminal> ::= "\"" \.*\ "\"" 
+// <character> ::= "<" \[a-zA-Z_]*\ ">"
+// ```
--- script/src/utility.ts
+++ script/src/utility.ts
@@ -1,11 +1,18 @@
-import { readFile as readFileRaw } from "node:fs/promises";
+// import { readFile as readFileRaw } from "node:fs/promises";
 
-export async function readFile(path: string): Promise<string | null> {
-    try {
-        return readFileRaw(process.cwd() + path, {
-            encoding: "utf8",
-        });
-    } catch (err) {
-        return null;
-    }
-}
+// export async function readFile(path: string): Promise<string | null> {
+//     try {
+//         return readFileRaw(process.cwd() + path, {
+//             encoding: "utf8",
+//         });
+//     } catch (err) {
+//         return null;
+//     }
+// }
+
+// export function toPascalCase(str: string) {
+//     return str
+//         .split(/[-_\s]+/)
+//         .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+//         .join('');
+// };
