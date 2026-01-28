6272ccc1ce4ef0f98aa770364cca2483ec64e712
senpuki1do <senpuki1do@gmail.com>
2026-01-28 14:22:55 +0000

    fix add normalization logic to hash method

--- src/utility/hashable_float.rs
+++ src/utility/hashable_float.rs
@@ -6,7 +6,7 @@ where
 
 impl<T> HashableFloat<T>
 where
-    T: std::fmt::Debug + Clone + Copy + PartialEq + PartialOrd + ToBits
+    T: std::fmt::Debug + Clone + Copy + PartialEq + PartialOrd
 {
     pub fn new(value: T) -> Self {
         HashableFloat(value)
@@ -17,24 +17,38 @@ where
     }
 }
 
-impl<T> std::hash::Hash for HashableFloat<T>
-where 
-    T: std::fmt::Debug + Clone + Copy + PartialEq + PartialOrd + ToBits
-{
+impl std::hash::Hash for HashableFloat<f32> {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        let float = self.0;
-        float.to_bits().hash(state)
+        if self.get() == 0.0 {
+            0.0f32.to_bits().hash(state)
+        } else if self.get().is_nan() {
+            f32::NAN.to_bits().hash(state)
+        } else {
+            f32::to_bits(self.get()).hash(state)
+        }
+    }
+}
+
+impl std::hash::Hash for HashableFloat<f64> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        if self.get() == 0.0 {
+            0.0f64.to_bits().hash(state)
+        } else if self.get().is_nan() {
+            f64::NAN.to_bits().hash(state)
+        } else {
+            f64::to_bits(self.get()).hash(state)
+        }
     }
 }
 
 impl<T> Eq for HashableFloat<T>
 where 
-    T: std::fmt::Debug + Clone + Copy + PartialEq + PartialOrd + ToBits
+    T: std::fmt::Debug + Clone + Copy + PartialEq + PartialOrd
 {}
 
 impl<T> From<T> for HashableFloat<T>
 where 
-    T: std::fmt::Debug + Clone + Copy + PartialEq + PartialOrd + ToBits
+    T: std::fmt::Debug + Clone + Copy + PartialEq + PartialOrd
 {
     fn from(value: T) -> Self {
         HashableFloat(value)
@@ -43,24 +57,9 @@ where
 
 impl<T> std::fmt::Display for HashableFloat<T>
 where 
-    T: std::fmt::Debug + Clone + Copy + PartialEq + PartialOrd + ToBits
+    T: std::fmt::Debug + Clone + Copy + PartialEq + PartialOrd
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "HashableFloat({:?})", self.0)
     }
-}
-
-pub trait ToBits {
-    type Bits: std::hash::Hash;
-    fn to_bits(&self) -> Self::Bits;
-}
-
-impl ToBits for f32 {
-    type Bits = u32;
-    fn to_bits(&self) -> u32 { f32::to_bits(*self) }
-}
-
-impl ToBits for f64 {
-    type Bits = u64;
-    fn to_bits(&self) -> u64 { f64::to_bits(*self) }
 }
