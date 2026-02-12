551b4e69d204e46b143b6052b68d749d7a54d656
senpuki1do <senpuki1do@gmail.com>
2026-02-12 13:45:57 +0000

    add delete build.rs

--- build.rs
+++
@@ -1,7 +0,0 @@
-/// The main function for the build script.
-///
-/// This build script is currently empty, but it can be used to
-/// perform various tasks before the main project is compiled,
-/// such as generating code, compiling C dependencies, or
-/// setting environment variables.
-fn main() {}
