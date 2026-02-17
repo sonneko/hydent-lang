3892ae20096a29f2658b92be5010ec2609fbbe46
senpuki1do <senpuki1do@gmail.com>
2026-02-17 14:20:49 +0000

    update order of declarations

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -1,6 +1,30 @@
 use crate::parser::errors::ParseErr;
 use crate::tokenizer::tokens::Token;
 
+pub trait ASTNode:
+    Copy + Clone + std::fmt::Debug + std::hash::Hash + PartialEq + Eq + 'static + Sized
+{
+    const SYNC_POINT_SETS: SyncPointBitMap;
+    fn get_error_situation(err: ParseErr) -> Option<Self>;
+
+    fn is_sync_point(token: Option<&Token>) -> bool {
+        let set = Self::SYNC_POINT_SETS;
+        if let Some(&token) = token {
+            match token {
+                Token::Keyword(keyword) => (1 << (keyword as u8)) & set.keywords != 0,
+                Token::Operator(operator) => (1 << (operator as u8)) & set.operators != 0,
+                Token::Delimiter(delimiter) => (1 << (delimiter as u8)) & set.delimiter != 0,
+                Token::Literal(_) => set.literals,
+                Token::Identifier(_) => set.identifier,
+                Token::EndOfFile => set.eof,
+                Token::Comment(_) => set.comments,
+            }
+        } else {
+            false
+        }
+    }
+}
+
 pub struct SyncPointBitMap {
     keywords: u64,
     operators: u64,
@@ -37,30 +61,6 @@ impl SyncPointBitMap {
             comments,
             identifier,
             eof,
-        }
-    }
-}
-
-pub trait ASTNode:
-    Copy + Clone + std::fmt::Debug + std::hash::Hash + PartialEq + Eq + 'static + Sized
-{
-    const SYNC_POINT_SETS: SyncPointBitMap;
-    fn get_error_situation(err: ParseErr) -> Option<Self>;
-
-    fn is_sync_point(token: Option<&Token>) -> bool {
-        let set = Self::SYNC_POINT_SETS;
-        if let Some(&token) = token {
-            match token {
-                Token::Keyword(keyword) => (1 << (keyword as u8)) & set.keywords != 0,
-                Token::Operator(operator) => (1 << (operator as u8)) & set.operators != 0,
-                Token::Delimiter(delimiter) => (1 << (delimiter as u8)) & set.delimiter != 0,
-                Token::Literal(_) => set.literals,
-                Token::Identifier(_) => set.identifier,
-                Token::EndOfFile => set.eof,
-                Token::Comment(_) => set.comments,
-            }
-        } else {
-            false
         }
     }
 }
