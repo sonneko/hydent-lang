f1c65a34ff4e198fb52d3479e90a9b1d6953f013
senpuki1do <senpuki1do@gmail.com>
2026-02-18 03:19:01 +0000

    add website basis by Astro

---
+++ website/astro.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Hydent PL',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/sonneko/hydent-lang' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'docs/get-started' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
