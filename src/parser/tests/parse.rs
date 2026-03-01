aa9c764133f7ba1d2b433afd283305ef920c6c47
senpuki1do <senpuki1do@gmail.com>
2026-03-01 14:54:20 +0000

    add big test about parser

--- src/parser/tests/parse.rs
+++ src/parser/tests/parse.rs
@@ -13,11 +13,12 @@ use crate::{
     tokenizer::{token_stream::TokenStream, tokenize::Tokenizer},
 };
 
-fn parse(source: &str) -> (Module, Arena, Arena) {
+fn parse(source: &str) {
     let source = String::from(source);
     let mut symbols = SymbolFactory::new(SourceHolder::new(&source));
     let tokenizer = Tokenizer::new(&source, &mut symbols);
     let (tokens, errors) = tokenizer.tokenize();
+    println!("tokenized.");
     let mut ast_arena = Arena::new();
     let mut errors_arena = Arena::new();
     let ast = {
@@ -33,12 +34,53 @@ fn parse(source: &str) -> (Module, Arena, Arena) {
         let ast = parser.parse();
         ast
     };
-    (ast.unwrap(), ast_arena, errors_arena)
+    println!("parsed.");
+    let ast = Ast::new(ast.unwrap(), ast_arena, SourceHolder::new(&source));
 }
 
 #[test]
+#[cfg(miri)]
 fn test_parse() {
-    let (ast, arena, _) = parse("fn main(){} fn d() {}");
-    let ast = Ast::new(ast, arena);
-    println!("\n\n{}", ast);
+    parse(
+        r#"
+import { Result, Ok, Err } from "std/result";
+import { Option, Some, None } from "std/option";
+
+class UserError {
+    message: String;
+
+    #summary "get user name"
+    #panics "content is empty"
+    try fn get_name(self): String {
+        if self.name.is_empty() {
+            panic("user is empty");
+        }
+        return self.name;
+    }
+
+    #summary "represents verified users"
+    #params name: "user name not empty"
+    #returns "new users or error"
+    #side_effects "print to std output"
+    pub fn new(name: String): Result<Self, UserError> {
+        if name.is_empty() {
+            return Err(UserError { message: "name is empty" });
+        }
+        std::io::println("verified user");
+        Ok(Self { name })
+    }
+}
+
+fn main() {
+    match User::new("Alice".to_string()) {
+        Ok(user) => {
+            let name = try user.get_name();
+            std::io::println("created users" + name);
+        },
+        Err(error) => {
+            std::io::println("fail to create users" + error.message);
+        },
+    }   
+    "#,
+    )
 }
