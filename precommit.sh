16cc53f30fecbd7ea6a2dd62038d42ea660713a2
senpuki1do <senpuki1do@gmail.com>
2026-03-05 04:55:09 +0000

    update precommit

--- precommit.sh
+++ precommit.sh
@@ -1,6 +1,12 @@
+cd script
+npm run self-test
+npm start -- ci
+npm run compiler-test
+cd ../
 cargo build
 cargo fmt --all -- --check
 cargo clippy -- -D warnings
 cargo miri setup || true
 cargo miri test --lib || true
-cargo test --verbose
+cargo test
+
